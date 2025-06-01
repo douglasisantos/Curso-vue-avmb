17367e3e5c877730556c29a3ae0171ef391098f1
douglasisantos <douglasiracet@gmail.com>
2025-06-01 19:25:59 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <PrimeiroComponente />
+</template>
+
+<script setup lang="ts">
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Olá Vue!</h1>
+    <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const nome = ref('Douglas')
+const profissao = ref('Programador')
+</script>
